88b2151cfae9a58b0d7f9a1e66437701b42f72a6
sarveshbhai321 <144299654+sarveshbhai321@users.noreply.github.com>
2024-02-09 11:06:43 +0530

    Update assign2.cpp

--- assign2.cpp
+++ assign2.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 struct great
 {
@@ -7,34 +7,70 @@ struct great
 };
 class landmarks
 {
-    public:
- landmarks * land[10];
+public:
+    landmarks *land[10];
     string source;
     string location;
-    landmarks* next;
-    landmarks *temp=NULL;
-    int i=0;
+    landmarks *next;
+    landmarks *temp = NULL;
+    landmarks *temp1[10];
+    int i = 0;
     void addnode()
     {
-        cout<<"enter the source";
-        cin>>source;
-        land[i]->source=source;
+        cout << "enter the source";
+        cin >> source;
+        landmarks *Node = new landmarks;
+        Node->source = source;
+        land[i] = Node;
         i++;
     }
     void createpath()
     {
-        cout<<"enter the source you wanna find";
-        cin>>source; 
-        cout<<"enter the destination";
-        cin>>location; 
-        landmarks*node=new landmarks;
-        for(int j=0;j<i;j++)
+        cout << "enter the source you wanna find";
+        cin >> source;
+        cout << "enter the destination";
+        cin >> location;
+        landmarks *node = new landmarks;
+        for (int j = 0; j < i; j++)
         {
-            if(land[i]->source==source)
-            {  
-                land[i]->next=node;
-                node->location=location;
+            if (land[j]->source == source)
+            {
+                landmarks *temp1[j];
+
+                temp1[j]->next = node;
+                node->location = location;
+                temp1[j] = node;
             }
         }
+    }
+    void display()
+    {
+        cout << "enter the source you wanna search";
+        cin >> source;
+        for (int j = 0; j < i; j++)
+        {
+            cout << "the source is" << source << "it has path with" << temp1[j]->location;
+        }
+    }
+};
+int main()
+{
+    landmarks obj;
+    int operators;
+    int k = 0;
+    while (k < 8)
+    {
+        cout << "enter the operator";
+        cin >> operators;
+        switch (operators)
+        {
+        case 1:
+            obj.addnode();
+            break;
+        case 2:
+            obj.createpath();
+            break;
+        }
+        k++;
     }
 }
