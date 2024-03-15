1f4010cdeb1bc91d4edce9d0451055aecb99a872
sarveshbhai321 <144299654+sarveshbhai321@users.noreply.github.com>
2024-03-15 11:46:43 +0530

    Add files via upload

---
+++ ass4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class bst
+{
+    bst* temp1;
+    bst* temp2;
+    bst* head=NULL;
+    string root;
+    void input()
+    {
+        cout<<"enter the root";
+        cin>>;
+    }
+
+}
